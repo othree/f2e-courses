95751e045606dd2f41fecd0b50bf5b196788131e
othree <othree@gmail.com>
2015-10-06 12:28:56 +0800

    Update nav sample

---
+++ 02.JSandBOMandDOM/nav/01.js
@@ -0,0 +1,9 @@
+
+window.onload = function () {
+  document.getElementById("nav-button").addEventListener('click', function () {
+  
+    document.getElementById('nav-list').className = 'active';
+  
+  }, false);
+};
+
